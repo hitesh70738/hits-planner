e5e9ba60c2b7aedc3162d1148f076edb35b87f08
Hitesh Patel <hitesh@Hiteshs-MacBook-Pro.local>
2023-12-12 19:31:30 +0000

    made some changes to the colour scheme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # hits-planner
+
+## Task
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Approach
+
+I used the activites and help from the assistance for this task. Going over the activites and doing some extra google search helped with this task. I used external websites like stack-overflow, mdm, and chatgpt etc. These tools helped me develop the code I have so far.
+
+## Github page-link
+
+https://hitesh70738.github.io/hits-planner/
+
+## Conclusion 
+This weeks challenege has been difficult to attempt. The logic makes sense and certain parts click. I need to study and practice more on how to save and get localstorage items. I understand the logic but the final piece is lacking some functionality of the site.
+
+## Screenshots 
--- script.js
+++ script.js
@@ -2,38 +2,71 @@ var today = dayjs();
 var todayDate = $('#currentDay').text(today.format("dddd - DD - MMMM - YYYY"))
 console.log(todayDate)
 
+var currentHour = dayjs().hour()
+
+
+
 function createTimeBlocks(){
-    const dayHours = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM']
+    const dayHours = [9, 10, 11,12,13,14,15,16,17,18,19,20,21]
+    // console.log('current hour = ' + currentHour)
+    // console.log('current hour = ' + dayHours[0])
+    // console.log('<', dayHours[8] < currentHour)
+    // console.log('>', dayHours[8] > currentHour)
+    // console.log('===', dayHours[8] === currentHour)
+
+
     for (var i = 0; i < dayHours.length;i++) {
         
         // const timeFrame = $('class', 'time-block').text(dayHours[i])
         // $('.container').append(timeFrame)
-        const time = $('<p>').attr('class','row')
-        const eventInput = $('<input class="event-input" type="text" placeholder="Add event here">');
-        const saveBtn = $('<button class="btn">Save</button>')
+        const textArea = $('<div class="subsection row"></div>')
+        const time = $('<p class="hour col-2"></p>')
+        const eventInput = $(`<textarea class="event-input description col-8 data-hour=${dayHours[i]} " type="text" placeholder="Add event here"></textarea>`)
+        const saveBtn = $('<button class="btn saveBtn col-2">Save</button>')
 
+    
         time.text(dayHours[i])
-        $('.container').append(time)
+        textArea.append(time)
+        textArea.append(eventInput)
+        textArea.append(saveBtn)
+        $('.container').append(textArea)
         
         // $('.event-input').css('background-color', '#ff6961')
-        time.append(eventInput)
+        // time.append(eventInput)
         // $('.btn').attr('class','saveBtn')
         // saveBtn.append(eventInput)
-        saveBtn.on('click', function(){
-            const input = eventInput.val();
-        })
-        
-        // if (i === 3){
-        //     $('.event-input').css('background-color', '#ff6961')
-        //     break;
-        // } else if (i === 4 || i === 9){
-        //     $('.event-input').css('background-color', '#77dd77')
-        // }
-    
-    }
+        // saveBtn.on('click', function(){
+        //     const input = eventInput.val();
+        // }) `$( "body" ).data( "foo", 52 );`
 
         
+        console.log(eventInput.attr( "data=[hour]"))
     
+        
+        if (eventInput.getAttribute('data-hour') < currentHour){
+            console.log(i, '<', dayHours[i] < currentHour)
+
+            $('.event-input').addClass('past')
+        }else if (dayHours[i] === currentHour){
+            $('.event-input').addClass('present')
+        }
+        else if (dayHours[i] > currentHour){ 
+            console.log(i, 'else', dayHours[i] > currentHour)
+            $('.event-input').addClass('future')
+        } else{
+            console.log('error')
+        }
+
+        saveBtn.on('click', function(event){
+            event.preventdefault()
+            const saveValue = eventInput.val();
+            localStorage.setItem('dayHours[i]', 'saveValue')
+            console.log(saveValue)
+    
+        })
+
+
+    }
 
     }
 
